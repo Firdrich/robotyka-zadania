996d645268d82b9034d9b3249c2533c459b3211e
Firdrich <araen123@gmail.com>
2018-10-30 16:23:21 +0100

    Copy of swiatek-login

---
+++ app.py
@@ -0,0 +1,106 @@
+from flask import Flask, session, redirect, url_for, request, render_template, flash
+from database import DataBase
+from profile import Profile
+import sys
+
+app = Flask(__name__)
+app.secret_key = "jdu7x3j8e83iej7eeh8e"
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	db = DataBase()
+	print(session['logged_in'], file=sys.stderr)
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		if db.login_to_account(username,password):
+			session['logged_in'] = True
+			session['username'] = request.form['username']
+			if db.get_user_admin(username) == 1:
+				session['admin'] = True
+			return render_template('index.html')
+		else:
+			session['logged_in'] = False
+			flash("You typed wrong username or password!")
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	db = DataBase()
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		if db.add_account(username,password,email):
+			flash("You were successfully registered!")
+			return render_template('register.html')
+		else:
+			flash("That user arleady exist")
+			return render_template('register.html')
+	return render_template('register.html')
+
+
+@app.route('/logout',methods=['GET'])
+def logut():
+	session['logged_in'] = False
+	return redirect(url_for('index'))
+
+@app.route('/settings',methods=['GET','POST'])
+def settings():
+	username = session['username']
+	db = DataBase()
+	profile = Profile(db.get_user_id(username))
+	if request.method == 'POST':
+		username = session['username']
+		if 'ProfileData' in request.form:
+			name = request.form.get('name')
+			surname = request.form.get('surname')
+			age = request.form.get('age')
+			sex = request.form.get('sex')
+			if name != "":
+				db.change_setting("name",name,username)
+			if surname != "":
+				db.change_setting("surname",surname,username)
+			if age != "":
+				db.change_setting("age",age,username)
+			if sex != "":
+				db.change_setting("sex",sex,username)
+				flash("Successfully changed informations")
+		if 'ChangeMail' in request.form:
+			newmail = request.form['email']
+			db.change_email(newmail,username)
+			flash("Successfully changed mail")
+		if 'ChangePassword' in request.form:
+			password = request.form['password']
+			newpassword = request.form['newpassword']
+			repeatpassword = request.form['repeatpassword']
+			if db.get_user_password(username) == password:
+				if newpassword == repeatpassword:
+					db.change_password(newpassword,username)
+					flash("Successfully changed password")
+				else:
+					flash("Passwords are not the same")
+			else:
+				print(db.get_user_password(username))
+				print(password)
+				flash("You typed wrong password")
+		return redirect(url_for('settings'))
+
+	return render_template('settings.html', profile = profile)
+
+@app.route('/admin', methods=['GET','POST'])
+def admin():
+    return render_template('index.html')
+#@app.route('/profiledata',methods=['GET','POST'])
+#def profiledata():
+
+if __name__ == '__main__':
+	app.secret_key = "jdu7x3j8e83iej7eeh8e"
+	app.run(port=5011)
---
+++ database.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+class DataBase():
+
+    def __init__(self):
+        self.conn = None
+        self.connect()
+        self.cursor = self.get_cursor()
+        self.conn.commit()
+
+    def connect(self):
+        self.conn = sqlite3.connect('database1.db')
+
+    def get_cursor(self):
+        return self.conn.cursor()
+
+    def get_account(self,username):
+        self.cursor.execute("SELECT * FROM users WHERE username=\"%s\"" % username)
+        user = self.cursor.fetchone()
+        return user
+
+    def get_user_username(self,username):
+        self.cursor.execute("SELECT username FROM users WHERE username=\"%s\"" % username)
+        username = self.cursor.fetchone()
+        return username
+
+    def get_user_id(self,username):
+        self.cursor.execute("SELECT user_id FROM users WHERE username=\"%s\"" % username)
+        user_id = self.cursor.fetchone()
+        return int(user_id[0])
+
+    def get_user_password(self,username):
+        self.cursor.execute("SELECT password FROM users WHERE username=\"%s\"" % username)
+        password = self.cursor.fetchone()
+        return str(password[0])
+
+    def get_user_admin(self,username): #check if user is admin
+        self.cursor.execute("SELECT admin FROM users WHERE username=\"%s\"" % username)
+        admin = self.cursor.fetchone()
+        return admin #returns either 0 or 1
+
+    def login_to_account(self,username,password):
+        user = self.get_account(username)
+        if user == None:
+            return False
+        elif str(user[1]) == username and str(user[2]) == password:
+            return True
+
+    def add_account(self,username,password,email):
+        if self.get_user_username(username) is not None:
+            return False
+        else:
+            self.cursor.execute("INSERT INTO users(username, password, email) VALUES(\"%s\", \"%s\", \"%s\")" % (username, password, email))
+            self.cursor.execute("INSERT INTO settings(user_id,name,surname,age,sex) VALUES(\"%i\",\"%s\",\"%s\",\"%i\",\"%s\")" % (self.get_user_id(username),"Name","Surname",0,"Sex"))
+            self.conn.commit()
+            return True
+
+    def change_setting(self,parameter,name,username):
+        if type(name) == int:
+            self.cursor.execute("UPDATE settings SET \"%s\"=\"%i\" WHERE user_id=\"%i\"" % (parameter,name,self.get_user_id(username)))
+        else:
+            self.cursor.execute("UPDATE settings SET \"%s\"=\"%s\" WHERE user_id=\"%i\"" % (parameter,name,self.get_user_id(username)))
+        self.conn.commit()   
+
+    def change_email(self,newmail,username):   
+        self.cursor.execute("UPDATE users SET email=\"%s\" WHERE username=\"%s\"" % (newmail,username))
+        self.conn.commit()
+
+    def change_password(self,newpassword,username):
+        self.cursor.execute("UPDATE users SET password=\"%s\" WHERE username=\"%s\"" % (newpassword,username))
+        self.conn.commit()
